a5689529ce9833d7f3724874b9eb5d27b8d06dbf
Morgan Zhang <Morgan@Morgans-MacBook-Pro.local>
2014-09-11 14:37:37 -0700

    V0.5 with many bugs

--- src/cs601/proxy/ClientHandler.java
+++ src/cs601/proxy/ClientHandler.java
@@ -1,6 +1,153 @@
+import java.net.*;
+import java.io.*;
+
 public class ClientHandler implements Runnable {
 	public static final int HTTP = 80;
 
 	protected boolean debug = true;
+
+	Socket clientSocket;
+
+	public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+        System.out.println("I'm created~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    @Override
+	public void run() {
+		try {
+			System.out.println("I'm running!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			String processedRequestString = processRequest(clientSocket);
+			String returnData = toHost(clientSocket, processedRequestString);
+			returnDataToBrowser(clientSocket, returnData);
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace(System.err);
+		}
+	}
+
+	void returnDataToBrowser(Socket clientSocket, String returnData) throws IOException {
+		System.out.println("[E]" + clientSocket);
+
+		OutputStream toClient = clientSocket.getOutputStream();
+		PrintStream toClientPOut = new PrintStream(toClient);
+
+		System.out.println("before r");
+		System.out.println(returnData);
+		System.out.println("after r");
+
+		toClientPOut.println(returnData);
+		toClient.close();
+		System.out.println("[F]");
+	}
+
+	String processRequest(Socket clientSocket) throws IOException {
+		System.out.println("[A inside] Start on port: " + clientSocket.getPort());
+		//OutputStream out = clientSocket.getOutputStream();
+		InputStream in = clientSocket.getInputStream();
+		DataInputStream din = new DataInputStream(in);
+		String line = din.readLine();
+
+		String processedLine = "";
+
+		while (!line.equals("")) {
+
+			String helpStringArray[] = line.split(":");
+			if( helpStringArray[0].equals("User-Agent") || helpStringArray[0].equals("Proxy-Connection") || helpStringArray[0].equals("Referer") ) {
+				//System.out.print("****");
+			}
+			else {
+				processedLine = processedLine + line + "\n";
+			}
+			System.out.println(line);
+			//System.out.println("--");
+			line = din.readLine();
+
+			// if( line.equals("") ) System.out.println(" >>>>> should end here");
+			// else System.out.println("<<< continue");
+		}
+
+		System.out.println("^^^^^^^^^ ^^^^^^");
+		System.out.println("[B inside] closing port: " + clientSocket.getPort());
+
+		//din.close();
+		//clientSocket.close();
+
+		//System.out.println(processedLine);
+
+		return processedLine;
+	}
+
+	String toHost(Socket clientSocket, String processedRequestString) throws IOException {
+		System.out.println("[C inside] Start on port: " + clientSocket.getPort());
+		String lines[] = processedRequestString.split("\n");
+		String firstLine = lines[0];
+
+		System.out.println(" #####  " + firstLine + " **** " );
+
+		String elements[] = firstLine.split(" ");
+		String elements2[] = elements[1].split("//");
+		String elements3[] = elements2[1].split("/");
+
+		String hostUrl = elements3[0];
+
+		System.out.println(":::::: " + hostUrl);
+
+		Socket toHostSocket = new Socket(hostUrl, 80);
+		OutputStream toHostOut = toHostSocket.getOutputStream();
+		PrintStream toHostPOut = new PrintStream(toHostOut);
+		
+		toHostPOut.println(processedRequestString);
+		
+
+		InputStream toHostIn = toHostSocket.getInputStream();
+		DataInputStream toHostDIn = new DataInputStream(toHostIn);
+		String line = toHostDIn.readLine();
+
+		String returnData = "";
+
+		int timer = 0;
+
+		while (true) {
+
+			// String helpStringArray[] = line.split(":");
+			// if( helpStringArray[0].equals("User-Agent") || helpStringArray[0].equals("Proxy-Connection") || helpStringArray[0].equals("Referer") ) {
+			// 	System.out.print("****");
+			// }
+			// else {
+			// 	processedLine = processedLine + "\n" + line;
+			// }
+			if( line.equals("")) timer++;
+			if( line.equals("") && timer == 2 ) break;
+
+			returnData = returnData + line + "\n";
+
+			System.out.println("-=> " + line);
+
+			line = toHostDIn.readLine();
+		}
+		System.out.println("end print\n\n\n\n\n\n");
+
+		toHostPOut.close();
+		toHostDIn.close();
+
+		System.out.println("[D inside] closing port: " + toHostSocket.getPort());
+
+		toHostSocket.close();
+
+		return returnData;
+	}
+
+
+ //    forwardRemoteDataToBrowser()
+	// getHeaders()
+	// makeUpstreamRequest()
+	// openUpstreamSocket()
+	// process()
+	// readLine()
+	// run()
+	// writeHeaders()
+
+
 
 }
--- src/cs601/proxy/ProxyServer.java
+++ src/cs601/proxy/ProxyServer.java
@@ -1,5 +1,43 @@
-public class ProxyServer {
-	public static final boolean SingleThreaded = false;
+import java.net.*;
+import java.io.*;
 
-	public static void main(String[] args) { }
+
+public class ProxyServer {
+	public static final boolean SingleThreaded = true;
+
+	public static void main(String[] args) throws Exception {
+		int portNumber = 8080;
+
+		if( args.length > 0 ) portNumber = Integer.valueOf(args[0]);
+
+		System.out.printf("\n\n\n\n\n\n aaa %d \n", portNumber);
+
+		ServerSocket s = new ServerSocket(portNumber);
+
+		while ( true ) {
+			System.out.println("wait for connect");
+
+
+
+			Socket channel = s.accept();
+
+			System.out.println(" socket: " + channel );
+
+			//Runnable handler = new ClientHandler(channel);
+			ClientHandler handler = new ClientHandler(channel);
+
+			if ( SingleThreaded ) {
+				System.out.println(" before run");
+				handler.run();
+				System.out.println(" after run");
+			}
+			else {
+				Thread t = new Thread(handler);
+				t.start();
+			}
+		}
+	}
+
+
+
 }
